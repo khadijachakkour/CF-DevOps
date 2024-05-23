2f75366c7ec1cb3a5d35a882e8959f32a123cd16
nom1 <chakkourk4@gmail.com>
2024-05-23 14:55:09 +0100

    update hello.java

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
 	public static void main(String[] args)
     {
         System.out.println("Bonjour et bon courage dans votre examen en DevOps");
-        System.out.println("Bonjour et bon courage dans votre examen en DevOps");
 
         
 
