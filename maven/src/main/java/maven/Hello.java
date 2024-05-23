48a13e4ee0569faa60da5bee51e9e3f7c97d212b
nom1 <chakkourk4@gmail.com>
2024-05-23 14:25:26 +0100

    first commit

---
+++ maven/src/main/java/maven/Hello.java
@@ -0,0 +1,11 @@
+package maven;
+
+public class Hello {	
+	public static void main(String[] args)
+    {
+        System.out.println("Bonjour et bon courage dans votre examen en DevOps");
+        
+
+    }
+
+}
